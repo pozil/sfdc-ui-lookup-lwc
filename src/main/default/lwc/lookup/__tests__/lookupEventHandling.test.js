7b745950803c908f0d51fd5aa7be316881ef052d
pozil <philippe.ozil@gmail.com>
2020-06-29 09:56:58 +0200

    Added event handling tests

--- src/main/default/lwc/lookup/__tests__/lookupEventHandling.test.js
+++ src/main/default/lwc/lookup/__tests__/lookupEventHandling.test.js
@@ -1,7 +1,7 @@
 import { createElement } from 'lwc';
 import Lookup from 'c/lookup';
 
-const SAMPLE_SEARCH = 'sample search';
+const SAMPLE_SEARCH = 'sample';
 const SAMPLE_SEARCH_ITEMS = [
     {
         id: 'id1',
@@ -19,8 +19,7 @@ const SAMPLE_SEARCH_ITEMS = [
 const ARROW_DOWN = 40;
 const ENTER = 13;
 
-// Helper function to wait until the microtask queue is empty. This is needed for promise
-// timing when calling imperative Apex.
+// Helper function to wait until the microtask queue is empty.
 function flushPromises() {
     // eslint-disable-next-line no-undef
     return new Promise((resolve) => setImmediate(resolve));
@@ -34,7 +33,7 @@ describe('c-lookup event handling', () => {
         }
     });
 
-    it('handleClearSingleSelection works', () => {
+    it('can clear selection when single entry', () => {
         // Create element
         const element = createElement('c-lookup', {
             is: Lookup
@@ -50,7 +49,7 @@ describe('c-lookup event handling', () => {
         expect(element.selection.length).toBe(0);
     });
 
-    it('handleClearSelectedItem works', () => {
+    it('can clear selection when multi entry', () => {
         // Create element
         const element = createElement('c-lookup', {
             is: Lookup
@@ -66,7 +65,39 @@ describe('c-lookup event handling', () => {
         expect(element.selection.length).toBe(1);
     });
 
-    it('supports keyboard selection', () => {
+    it('can select item with mouse', () => {
+        jest.useFakeTimers();
+
+        // Create element with search handler
+        const element = createElement('c-lookup', {
+            is: Lookup
+        });
+        const searchFn = (event) => {
+            event.target.setSearchResults(SAMPLE_SEARCH_ITEMS);
+        };
+        element.addEventListener('search', searchFn);
+        document.body.appendChild(element);
+
+        // Set search term and force input change
+        const searchInput = element.shadowRoot.querySelector('input');
+        searchInput.value = SAMPLE_SEARCH;
+        searchInput.dispatchEvent(new CustomEvent('input'));
+
+        // Disable search throttling
+        jest.runAllTimers();
+
+        return flushPromises().then(() => {
+            // Simulate mouse selection
+            const searchResultItem = element.shadowRoot.querySelector('span[role=option]');
+            searchResultItem.click();
+
+            // Check selection
+            expect(element.selection.length).toBe(1);
+            expect(element.selection[0].id).toBe(SAMPLE_SEARCH_ITEMS[0].id);
+        });
+    });
+
+    it('can select item with keyboard', () => {
         jest.useFakeTimers();
 
         // Create element with search handler
@@ -95,6 +126,7 @@ describe('c-lookup event handling', () => {
 
             // Check selection
             expect(element.selection.length).toBe(1);
+            expect(element.selection[0].id).toBe(SAMPLE_SEARCH_ITEMS[0].id);
         });
     });
 });

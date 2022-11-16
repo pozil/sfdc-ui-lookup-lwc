905243260afe4aaba1eb77e885c1fc398e362a4f
pozil <pozil@salesforce.com>
2022-11-16 15:30:38 +0100

    feat: improved a11y with tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
         '^lightning/navigation$': '<rootDir>/jest-mocks/lightning/navigation'
     },
-    testMatch: ['**/__tests__/**/*.test.js']
+    testMatch: ['**/__tests__/**/*.test.js'],
+    setupFilesAfterEnv
 };
